e1a538ce411f4439d8b0d1086c0ce5ef2edf0ec3
saattrupdan <saattrupdan@gmail.com>
2022-06-21 08:35:47 +0200

    style: Rename main config to config.yaml

--- {{cookiecutter.package_name}}/src/{{cookiecutter.package_name}}/demo.py
+++ {{cookiecutter.package_name}}/src/{{cookiecutter.package_name}}/demo.py
@@ -5,13 +5,9 @@ from omegaconf import DictConfig
 from pathlib import Path
 
 
-@hydra.main(config_path="../config", config_name='main')
+@hydra.main(config_path="config", config_name='config')
 def demo_function(config: DictConfig):
     """A demo function."""
 
     raw_path = Path(config.raw.path)
     print(f"Process data using {raw_path}")
-
-
-if __name__ == '__main__':
-    demo_function()
