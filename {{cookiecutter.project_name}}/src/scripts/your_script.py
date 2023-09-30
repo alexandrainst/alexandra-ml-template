a902f9210178ad09af95c50c10f5bdd1e33c91d3
saattrupdan <saattrupdan@gmail.com>
2023-09-30 16:08:30 +0200

    docs: Better docstrings

--- {{cookiecutter.project_name}}/src/scripts/your_script.py
+++ {{cookiecutter.project_name}}/src/scripts/your_script.py
@@ -1,4 +1,8 @@
-"""Main script for your project."""
+"""Main script for your project.
+
+Usage:
+    python src/scripts/your_script.py <config_key>=<config_value> ...
+"""
 
 import hydra
 from omegaconf import DictConfig
@@ -8,7 +12,11 @@ from {{ cookiecutter.project_name }}.your_module import example_function
 
 @hydra.main(config_path="../../config", config_name="config", version_base=None)
 def main(config: DictConfig) -> None:
-    """Main function for your project."""
+    """Main function for your project.
+
+    Args:
+        config: The Hydra config for your project.
+    """
     example_function(config=config)
 
 
--- {{cookiecutter.project_name}}/src/{{cookiecutter.project_name}}/your_module.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.project_name}}/your_module.py
@@ -4,6 +4,10 @@ from omegaconf import DictConfig
 
 
 def example_function(config: DictConfig) -> None:
-    """An example function for your project."""
+    """An example function for your project.
+
+    Args:
+        config: The Hydra config for your project.
+    """
     print("Hello World!")
     print(f"Your config is: {config}")
