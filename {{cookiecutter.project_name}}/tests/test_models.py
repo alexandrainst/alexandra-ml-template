f882fdc24c254df9a7647528c234336d39231bb2
bourdeet <27770178+bourdeet@users.noreply.github.com>
2024-02-21 18:01:06 +0100

    more docstring changes

--- {{cookiecutter.project_name}}/tests/test_models.py
+++ {{cookiecutter.project_name}}/tests/test_models.py
@@ -1,13 +1,11 @@
 """Example test script for the project's
-model class
+model class.
 """
 import torch
-
 from {{ cookiecutter.library_name }}.ml_tools.models import {{ cookiecutter.class_prefix }}AE
 from {{ cookiecutter.library_name }}.ml_tools.models import {{ cookiecutter.class_prefix }}LSTM
 
 def test_lstm():
-
 	m = {{ cookiecutter.class_prefix }}LSTM(predict_window=1, lookback_window=4)
 	x_example = torch.Tensor([4.0, 35.0, 3.0, -4])
 	x_example = torch.unsqueeze(x_example, dim=0)
@@ -15,9 +13,7 @@ def test_lstm():
 	print(m(x_example))
 
 
-
 def test_autoencoder():
-
 	m = {{ cookiecutter.class_prefix}}AE(input_window=100, input_dims=2, latent_dims=33)
 	print("model:\n------------------\n", m)
 	# example of a batch of 34 input time series
@@ -31,6 +27,7 @@ def test_autoencoder():
 		f"\n-------------------\n\n \
 		decoder model output shape: {m.decoder(latent_x).shape}"
 	)
+
 
 if __name__ == "__main__":
 	test_autoencoder()
