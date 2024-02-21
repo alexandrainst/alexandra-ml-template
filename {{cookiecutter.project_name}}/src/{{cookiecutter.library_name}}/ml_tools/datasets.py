e02db43c138dbbe6afa5368173d4286e9ccd177e
bourdeet <27770178+bourdeet@users.noreply.github.com>
2024-02-21 17:43:02 +0100

    fixing docstrings and code styling issue

--- {{cookiecutter.project_name}}/src/{{cookiecutter.library_name}}/ml_tools/datasets.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.library_name}}/ml_tools/datasets.py
@@ -3,12 +3,22 @@ pytorch datasets used for training in this case.
 
 """
 import os
+import logging
 from typing import Any
 
 import numpy as np
 import torch
 from torch.utils.data import Dataset
 
+logger = logging.getLogger("ml_tools.datasets")
+
+# We iterate through this list of keys to
+# ensure the ordering stays consistent.
+# this might be superfluous
+ORDERED_VARIABLES = [
+"y",
+"state"
+]
 
 class {{ cookiecutter.class_prefix }}Dataset(Dataset):
     def __init__(
@@ -55,6 +65,110 @@ class {{ cookiecutter.class_prefix }}Dataset(Dataset):
 
 
 #########################
+def retrieve_data_from_sql(start_date: str, end_date: str) -> Any:
+    """Function that extracts raw data from postgres"""
+
+    # # connect to database
+    session = load_session()
+
+    statement = text(
+        f"""SELECT
+                time,
+                y
+            FROM
+                clean_example_data
+            WHERE 
+                time BETWEEN '{start_date}' AND '{end_date}'
+            ORDER BY 1;
+        """
+    )
+
+    returned = session.execute(statement)
+    data: OrderedDict[str, Any] = OrderedDict()
+
+    for i, x in enumerate(returned):
+        row = dict(x._mapping)
+        for k, v in row.items():
+            if i == 0:
+                data[k] = []
+            data[k].append(v)
+    return data
+
+
+
+def produce_snippets(
+    df: dict, time_window: int, include_keys: list = ORDERED_VARIABLES
+) -> Any:
+    """This function takes a historical dataset, and cuts it into
+    snippets of specified dimensions
+
+    The snippets can be taken defined as past or future observation,
+    depending on the sign of the time_window variable. In both cases,
+    the "current" time point is included in the array
+
+    Parameters
+    ----
+    df : dict
+        dictionary of time series data
+
+    time_window: int
+        number of time steps to keep. negative means past values
+
+    include_keys: list
+                    a list of keys we want to include in the
+                    snippet
+
+    Returns
+    ---
+
+    snippets : list
+        a list of produced snippets of data
+
+    current_pt : list
+        a list of the values defined as the "current" value
+        associated with the snippet
+    """
+
+    n_data_points = len(df[list(df.keys())[0]])
+
+    i = 0
+    current_pts: List[Any] = []
+    snippets: List[Any] = []
+    while i < n_data_points:
+        if time_window < 0 and i < (-time_window):
+            logger.debug(f"{i}: lower than time window {time_window}")
+            i += 1
+            continue
+
+        if time_window >= 0 and i > (n_data_points - time_window):
+            logger.debug(
+                f"{i}: higher than n_datapoints - time window \
+                {n_data_points-time_window}"
+            )
+            break
+
+        current_pt: dict[Any, Any] = {}
+        snippet: dict[Any, Any] = {}
+
+        for k in include_keys:
+            v = df[k]
+            if time_window < 0:
+                subv = v[(i + time_window) : i]
+            else:
+                subv = v[i + 1 : (i + 1 + time_window)]
+
+            current_pt[k] = [v[i]]
+            snippet[k] = subv
+
+        if len(snippet[list(snippet.keys())[0]]) == 0:
+            break
+        snippets.append(snippet)
+        current_pts.append(current_pt)
+        i += 1
+
+    return current_pts, snippets
+
+
 def normalize_data(df: dict[Any, Any]) -> dict[Any, Any]:
     """Placeholder function
 
@@ -62,6 +176,6 @@ def normalize_data(df: dict[Any, Any]) -> dict[Any, Any]:
     transformation to your data like 
     normalizing values between -1 and 1
     """
-    pass
+    return df
 
 #########################
--- {{cookiecutter.project_name}}/tests/test_models.py
+++ {{cookiecutter.project_name}}/tests/test_models.py
@@ -1,13 +1,14 @@
 """Example test script for the project's
 model class
 """
+import torch
 
 from {{ cookiecutter.library_name }}.ml_tools.models import {{ cookiecutter.class_prefix }}AE
 from {{ cookiecutter.library_name }}.ml_tools.models import {{ cookiecutter.class_prefix }}LSTM
 
 def test_lstm():
 
-	m = {{ cookiecutter.library_name }}LSTM(predict_window=1, lookback_window=4)
+	m = {{ cookiecutter.class_prefix }}LSTM(predict_window=1, lookback_window=4)
 	x_example = torch.Tensor([4.0, 35.0, 3.0, -4])
 	x_example = torch.unsqueeze(x_example, dim=0)
 	x_example = torch.unsqueeze(x_example, dim=0)
@@ -17,7 +18,7 @@ def test_lstm():
 
 def test_autoencoder():
 
-	m = {{ cookiecutter.library_name }}AE(input_window=100, input_dims=2, latent_dims=33)
+	m = {{ cookiecutter.class_prefix}}AE(input_window=100, input_dims=2, latent_dims=33)
 	print("model:\n------------------\n", m)
 	# example of a batch of 34 input time series
 	x = torch.zeros([34, 1, 200])
