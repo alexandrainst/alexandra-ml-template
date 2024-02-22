4f461bc75dfacb54b3f9a9e5ac92d7423c6fe0c3
bourdeet <27770178+bourdeet@users.noreply.github.com>
2024-02-22 15:11:41 +0100

    docstrings on ml tools

--- {{cookiecutter.project_name}}/src/{{cookiecutter.library_name}}/ml_tools/models.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.library_name}}/ml_tools/models.py
@@ -18,111 +18,111 @@ logger = logging.getLogger(__name__)
 # Example LSTM Cell
 #
 class {{ cookiecutter.class_prefix }}LSTM(nn.Module):
-	def __init__(self, lookback_window, predict_window, n_hidden=None):
-		"""Univariate LSTM prediction model.
+    def __init__(self, lookback_window, predict_window, n_hidden=None):
+        """Univariate LSTM prediction model.
 
-		Parameters:
-		---
+        Parameters:
+        ---
 
-		lookback_window: int
-						number of earlier time steps per sequence
+        lookback_window: int
+            number of earlier time steps per sequence
 
-		predict_window: int
-						number of steps ahead the prediciton must cover
-		"""
-		super({{ cookiecutter.class_prefix }}LSTM, self).__init__()
+        predict_window: int
+            number of steps ahead the prediciton must cover
+        """
+        super({{ cookiecutter.class_prefix }}LSTM, self).__init__()
 
-		if n_hidden is None:
-			n_hidden = lookback_window
-		self.lstm = nn.LSTM(input_size=lookback_window, hidden_size=n_hidden)
-		self.linear = nn.Linear(n_hidden, predict_window)
+        if n_hidden is None:
+            n_hidden = lookback_window
+        self.lstm = nn.LSTM(input_size=lookback_window, hidden_size=n_hidden)
+        self.linear = nn.Linear(n_hidden, predict_window)
 
-	def forward(self, x):
-		"""Forward pass on the model."""
-		output, (final_hidden_state, final_cell_state) = self.lstm(x)
-		x = self.linear(output)
-		return x
+    def forward(self, x):
+        """Forward pass on the model."""
+        output, (final_hidden_state, final_cell_state) = self.lstm(x)
+        x = self.linear(output)
+        return x
 
 
 #
 # Autoencoders for anomaly detection
 #
 class {{ cookiecutter.class_prefix }}Encoder(nn.Module):
-	def __init__(self, input_dims, latent_dims):
-		"""encoder section of autoencoder model."""
-		super({{ cookiecutter.class_prefix }}Encoder, self).__init__()
-		self.linear1 = nn.Linear(input_dims, 56)
-		self.linear2 = nn.Linear(56, latent_dims)
+    def __init__(self, input_dims, latent_dims):
+        """encoder section of autoencoder model."""
+        super({{ cookiecutter.class_prefix }}Encoder, self).__init__()
+        self.linear1 = nn.Linear(input_dims, 56)
+        self.linear2 = nn.Linear(56, latent_dims)
 
-	def forward(self, x):
-		"""Forward pass on the model."""
-		x = torch.flatten(x, start_dim=1)
-		x = F.relu(self.linear1(x))
-		return self.linear2(x)
+    def forward(self, x):
+        """Forward pass on the model."""
+        x = torch.flatten(x, start_dim=1)
+        x = F.relu(self.linear1(x))
+        return self.linear2(x)
 
 
 class {{ cookiecutter.class_prefix }}Decoder(nn.Module):
-	def __init__(self, latent_dims, output_dims):
-		"""decoder section of autoencoder model."""
-		super({{ cookiecutter.class_prefix }}Decoder, self).__init__()
-		self.linear1 = nn.Linear(latent_dims, 56)
-		self.linear2 = nn.Linear(56, output_dims)
+    def __init__(self, latent_dims, output_dims):
+        """decoder section of autoencoder model."""
+        super({{ cookiecutter.class_prefix }}Decoder, self).__init__()
+        self.linear1 = nn.Linear(latent_dims, 56)
+        self.linear2 = nn.Linear(56, output_dims)
 
-	def forward(self, x):
-		"""Forward pass on the model."""
-		x = torch.flatten(x, start_dim=1)
-		x = F.relu(self.linear1(x))
-		return self.linear2(x)
+    def forward(self, x):
+        """Forward pass on the model."""
+        x = torch.flatten(x, start_dim=1)
+        x = F.relu(self.linear1(x))
+        return self.linear2(x)
 
 
 class {{ cookiecutter.class_prefix }}AE(nn.Module):
-	def __init__(self, input_dims, input_window, latent_dims):
-		"""Autoencoder model for anomaly detection."""
-		super({{ cookiecutter.class_prefix }}AE, self).__init__()
-		input_size = input_dims * input_window
-		self.encoder = {{ cookiecutter.class_prefix }}Encoder(input_size, latent_dims)
-		self.decoder = {{ cookiecutter.class_prefix }}Decoder(latent_dims, input_size)
+    def __init__(self, input_dims, input_window, latent_dims):
+        """Autoencoder model for anomaly detection."""
+        super({{ cookiecutter.class_prefix }}AE, self).__init__()
+        input_size = input_dims * input_window
+        self.encoder = {{ cookiecutter.class_prefix }}Encoder(input_size, latent_dims)
+        self.decoder = {{ cookiecutter.class_prefix }}Decoder(latent_dims, input_size)
 
-	def forward(self, x):
-		"""Forward pass on the model."""
-		z = self.encoder(x)
-		return self.decoder(z)
+    def forward(self, x):
+        """Forward pass on the model."""
+        z = self.encoder(x)
+        return self.decoder(z)
 
 
 #
 # Various types of loss functions
 #
 class CustomLoss(nn.Module):
-	def __init__(self):
-		"""Custom loss for output predictor.
+    def __init__(self):
+        """Custom loss for output predictor.
 
-		Calculate the maximum value of the last 
-		three elements in the input sequence
-		"""
-		super(CustomLoss, self).__init__()
+        Calculate the maximum value of the last 
+        three elements in the input sequence
+        """
+        super(CustomLoss, self).__init__()
 
-	def forward(self, predicted_output, input_sequence, true_output):
-		"""Forward pass on the model."""
-		last_three_max = torch.max(input_sequence[:, -10:], true_output)
+    def forward(self, predicted_output, input_sequence, true_output):
+        """Forward pass on the model."""
+        last_three_max = torch.max(input_sequence[:, -10:], true_output)
 
-		# Compute the squared difference between the predicted output and last_three_max
-		loss = torch.sum((predicted_output - last_three_max) ** 2)
+        # Compute the squared difference between the predicted output and last_three_max
+        loss = torch.sum((predicted_output - last_three_max) ** 2)
 
-		return loss
+        return loss
 
 
 class AsymmetricLoss(nn.Module):
-	def __init__(self):
-		"""Loss function that over penalizes underpredictions."""
-		super(AsymmetricLoss, self).__init__()
+    def __init__(self):
+        """Loss function that over penalizes underpredictions."""
+        super(AsymmetricLoss, self).__init__()
 
-	def forward(self, predicted_output, input_sequence, true_output):
-		"""Forward pass on the model."""
-		E = (predicted_output - true_output) / true_output
-		negative = 100 * E[E < 0.0] ** 2.0
-		positive = torch.abs(E[E > 0.0])
-		loss = negative.mean() + positive.mean()
+    def forward(self, predicted_output, input_sequence, true_output):
+        """Forward pass on the model."""
+        E = (predicted_output - true_output) / true_output
+        negative = 100 * E[E < 0.0] ** 2.0
+        positive = torch.abs(E[E > 0.0])
+        loss = negative.mean() + positive.mean()
 
-		return loss
+        return loss
 
 
