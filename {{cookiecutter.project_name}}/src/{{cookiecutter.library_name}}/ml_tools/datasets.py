6931fb6023a9ced65ff927b1d30dfa07530483bf
bourdeet <27770178+bourdeet@users.noreply.github.com>
2024-03-07 15:43:52 +0100

    updates to traintest and datasets, with improve handling of normalization + training logs and documentation

--- {{cookiecutter.project_name}}/src/{{cookiecutter.library_name}}/ml_tools/datasets.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.library_name}}/ml_tools/datasets.py
@@ -8,6 +8,7 @@ import os
 from collections import OrderedDict
 from typing import Any, List
 
+import numpy as np
 import torch
 from {{cookiecutter.library_name}}.utils.sql import load_session
 from sqlalchemy.sql import text
@@ -26,25 +27,28 @@ class {{ cookiecutter.class_prefix }}Dataset(Dataset):
 
     def __init__(
         self,
-        model_params: dict[str, Any],
+        static_values: dict[str, Any],
         dataset_path: str,
     ) -> None:
         """Initialize the dataset class.
 
         Parameter
         -------
-        model_type: str
-            defines how the data is split
+        static_values: dict
+            Statistical properties related to an entire dataset.
+            These are passed into the normalize_data function
 
         dataset_path : str
             path to a directory containing .pt files
 
         """
         self.dataset_path = dataset_path
-        self.model_params = model_params
+        self.static_values = static_values
 
         all_files = []
         for element in os.listdir(dataset_path):
+            if "dataset_statistics" in element:
+                continue
             all_files.append(element)
         self.snippets = sorted(all_files)
 
@@ -63,15 +67,28 @@ class {{ cookiecutter.class_prefix }}Dataset(Dataset):
 
         # The snippet tensor dimension should have size
         data = torch.load(os.path.join(self.dataset_path, self.snippets[idx]))
-
-        out = data.flatten()
-
+        
+        # normalize data
+        normalized_data = normalize_data(data, static_values=self.static_values)
+        normalized_data = torch.Tensor(np.array(list(normalized_data.values())))
+        out = normalized_data.flatten()
         return out
 
 
-#########################
-def retrieve_data_from_sql(start_date: str, end_date: str) -> Any:
-    """Function that extracts raw data from postgres."""
+###############################################################
+# Data extraction and preprocessing
+
+def retrieve_data_from_sql(sql_table: str, start_date: str, end_date: str) -> Any:
+    """Function that extracts raw data from postgres.
+
+    In addition to the variables extracted from postgres,
+    This function also creates separate variables for time
+    components such as the second, minute, hour and day
+    of the week.
+
+    Feel free to add / remove these depending on the 
+    resolution and periodicity of your time series.
+    """
     session = load_session()
 
     statement = text(
@@ -79,7 +96,7 @@ def retrieve_data_from_sql(start_date: str, end_date: str) -> Any:
                 time,
                 y
             FROM
-                clean_example_data
+                {sql_table}
             WHERE 
                 time BETWEEN '{start_date}' AND '{end_date}'
             ORDER BY 1;
@@ -89,14 +106,52 @@ def retrieve_data_from_sql(start_date: str, end_date: str) -> Any:
     returned = session.execute(statement)
     data: OrderedDict[str, Any] = OrderedDict()
 
+    data["month"] = []
+    data["day"] = []
+    data["weekday"] = []
+    data["hour"] = []
+    data["minute"] = []
     for i, x in enumerate(returned):
         row = dict(x._mapping)
         for k, v in row.items():
             if i == 0:
                 data[k] = []
             data[k].append(v)
+
+            if k == "time":
+                data["month"].append(v.month)
+                data["day"].append(v.day)
+                data["weekday"].append(v.weekday())
+                data["hour"].append(v.hour)
+                data["minute"].append(v.minute)
+            
     return data
 
+
+def gather_dataset_statistics(df: dict[Any, Any])-> dict[Any, Any]:
+    """Compute statistical properties of individual dataset.
+
+    This function looks at all dataset variables, and saves
+    some of their properties in static_values, which can
+    later be used alongside the normalizing function
+    """
+
+    static_values: dict[str, Any]= {}
+
+    for k, v in df.items():
+        if k=="time":
+            continue
+        v = [e if e is not None else 0.0 for e in v]
+        v = np.array(v, dtype=float)
+        static_values[k] = {
+        "mean": np.nanmean(v),
+        "std": np.nanstd(v, ddof=1),
+        "min": np.nanmin(v),
+        "max": np.nanmax(v),
+        "median":np.nanmedian(v)
+        }
+
+    return static_values
 
 def produce_snippets(
     df: dict, time_window: int, include_keys: list = ORDERED_VARIABLES
@@ -168,13 +223,39 @@ def produce_snippets(
     return current_pts, snippets
 
 
-def normalize_data(df: dict[Any, Any]) -> dict[Any, Any]:
-    """Placeholder function.
+def normalize_data(df: dict[Any, Any], static_values: dict[Any, Any]) -> dict[Any, Any]:
+    """Normalize data, if needed with static values.
 
     Use this function to apply simple
     transformation to your data like
     normalizing values between -1 and 1
+
+    Parameters:
+    ---
+
+    df : dict
+        Raw dataset extracted from retrieve_from_sql
+
+    static_values: dict
+        dictionary of precalculated normalization values,
+        usually based on the training dataset
     """
+
+    for k, v in df.items():
+        if k == "time":
+            continue
+
+        # Clear None's and convert to NaN
+        v = [e if e is not None else 0.0 for e in v]
+        v = np.array(v, dtype=float)
+
+        if k == "some_large_variable":
+            v = np.log10(v + 1)
+
+        mu = static_values[k]["mean"]
+        sigma = static_values[k]["std"]
+        df[k] = (v - mu) / sigma
+
     return df
 
 
--- {{cookiecutter.project_name}}/src/{{cookiecutter.library_name}}/ml_tools/traintest.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.library_name}}/ml_tools/traintest.py
@@ -3,16 +3,19 @@
 They classes are used to schedule and coordinate an ML
 training phase.
 """
-
+import logging
 import torch
 from torch.utils.data import DataLoader
+from typing import Any
 
+logger = logging.getLogger("ml_tools.traintest")
 
 # Generic Train Test class. avoid changing this as much as possible
-class AlgoTrainTest:
+class AlgoTraining:
     """Wrapper class around training steps of a pytorch model."""
 
-    def __init__(self, model, optimizer, loss_fn, device="cuda"):
+    def __init__(self,
+        model, optimizer, loss_fn, device):
         """Initialize the training class."""
         self.model = model.to(device)
         self.loss = loss_fn
@@ -20,25 +23,20 @@ class AlgoTrainTest:
         self.device = device
         self.datasets = {}
         self.current_dataset = None
-        self.rms_history = []
-        self.gradients_history = []
 
         # In debug mode, fix the random seed
-        # torch.manual_seed(0)
-        # also avoid using non-deterministic algorithms when running  fx. convolutions:
-        # torch.use_deterministic_algorithms(True)
+        if logger.level==logging.DEBUG:
+            logger.debug("Running in debug mode. Avoiding randomness as much as possible...")
+            torch.manual_seed(0)
+            # also avoid using non-deterministic algorithms when running  fx. convolutions:
+            torch.use_deterministic_algorithms(True)
 
         # record historic evolution of the training
+        self._setup_training_logs()
+
+    def _setup_training_logs(self):
+        """Setup structure of training results and specific variables to track."""
         self.loss_history = []
-        self.weights_history = []
-        self.bias_history = []
-
-        # Linear layer weights
-        self.linweights = []
-        self.linbias = []
-
-        # store results of an evaluation
-        self.eval_results = {}
 
     def add_dataset(self, label, dataset, batch_size=10):
         """Import new pytorch datasets and load them as DataLoders."""
@@ -69,8 +67,6 @@ class AlgoTrainTest:
         """Function that calls the training step over a particular dataloader."""
         self.current_dataset = dataset_label
         self.loss_history = []
-        self.rms_history = []
-        self.gradients_history = []
 
         for i in range(n_epochs):
             if (i + 1) % 10 == 0:
@@ -82,25 +78,31 @@ class AlgoTrainTest:
         if autosave:
             self.save_trained_model()
 
-    def save_trained_model(self, output_name: str | None = None) -> None:
-        """Save the trained model to .pt format.
+    def record_session(self, output_prefix: str | None=None):
+        """Save model + training metadata to file."""
 
-        - batch size
-        - dataset name
-        - learning rate
-        - n_epochs
-        """
-        model_name = type(self.model).__name__
-        loss_name = type(self.loss).__name__
-        optim_name = type(self.optim).__name__
+        if output_prefix is None:
+            model_name = type(self.model).__name__
+            loss_name = type(self.loss).__name__
+            optim_name = type(self.optim).__name__
+            output_prefix = f"{model_name}_{loss_name}_{optim_name}_\
+            trained_on_{self.current_dataset}"
 
-        if output_name is None:
-            full_name = f"{model_name}_{loss_name}_{optim_name}_\
-            trained_on_{self.current_dataset}.pt"
-        else:
-            full_name = output_name
+        self.save_trained_model(output_name=output_prefix+'.pt')
+        self.save_training_metadata(output_name=output_prefix+'_metadata.pt')
 
-        torch.save(self.model.state_dict(), full_name)
+
+    def save_trained_model(self, output_name: str) -> None:
+        """Save the trained model to .pt format."""
+        torch.save(self.model.state_dict(), output_name)
+        logger.info(f"Saved model to {output_name}.")
+
+    def save_training_metadata(self, output_name: str)-> None:
+        """Save the training metadata (fx. the loss function history)."""
+        metadata = {"loss_history": self.loss_history}
+        torch.save(metadata, output_name)
+        logger.info(f"Saved model metadata to {output_name}.")
+
 
 
 #####################################################
@@ -108,7 +110,7 @@ class AlgoTrainTest:
 # the training algorithm to your needs
 
 
-class {{cookiecutter.class_prefix}}TrainTest(AlgoTrainTest):
+class {{cookiecutter.class_prefix}}Training(AlgoTraining):
     """In case a project requires customize training.
 
     If the particular project needs more custom training style, it
@@ -129,10 +131,13 @@ class {{cookiecutter.class_prefix}}TrainTest(AlgoTrainTest):
         device="cuda",
     ):
         """Initialize inherited class + extra parameters."""
-        AlgoTrainTest.__init__(
+        AlgoTraining.__init__(
             self, model=model, optimizer=optimizer, loss_fn=loss_fn, device=device
         )
         self.model_type = model_type
+        # Example where we want to record PCA fits of an encoder model
+        self.pca_fit = None
+        self.pca_data = []
 
     def _train_one_epoch(self):
         """Override of the generic training.
@@ -166,3 +171,46 @@ class {{cookiecutter.class_prefix}}TrainTest(AlgoTrainTest):
 
             # run a step of optimization
             self.optim.step()
+
+    def fit_pca_to_dataset(self, dataset_label: str="train") -> dict[Any, Any]:
+        """Perform Principal Component Analysis of a dataset."""
+        from sklearn.decomposition import PCA
+        import numpy as np
+
+        latent_data = []
+        self.model.to("cpu")
+        self.model.eval()
+        
+        #Extract the latent output from the encoder part of the model
+        for d in self.datasets[dataset_label]:
+            latent_representation = self.model.encoder(d)
+            array = latent_representation.cpu().detach().numpy()
+            latent_data.append(array)
+
+        latent_data = np.vstack(latent_data)
+
+        #
+        # Determine the PCA of the training sample in latent space
+        #
+        pca = PCA(n_components=2, svd_solver="full")
+        self.pca_fit = pca.fit(latent_data)
+        self.pca_data = self.pca_fit.transform(latent_data)
+
+    def record_session(self, output_prefix: str):
+        """Save model + training metadata to file."""
+        if output_prefix is None:
+            model_name = type(self.model).__name__
+            loss_name = type(self.loss).__name__
+            optim_name = type(self.optim).__name__
+            output_prefix = f"{model_name}_{loss_name}_{optim_name}_\
+            trained_on_{self.current_dataset}"
+
+        self.save_trained_model(output_name=output_prefix+'.pt')
+        self.save_training_metadata(output_name=output_prefix+'_metadata.pt')
+        self.save_pca(output_name=output_prefix+'_pca.pt')
+
+    def save_pca(self, output_name: str)-> None:
+        """Save the PCA fit + data from the training set"""
+        self.fit_pca_to_dataset(dataset_label="train")
+        torch.save([self.pca_fit, self.pca_data], output_name)
+        logger.info(f"Saved PCA data to {output_name}.")
