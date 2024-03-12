0e0406471e9133d398c628456d241f4303a0edaf
bourdeet <27770178+bourdeet@users.noreply.github.com>
2024-03-12 12:59:38 +0100

    removing a file that gets created during install

--- {{cookiecutter.project_name}}/src/{{cookiecutter.project_name}}/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-.. include:: ../../README.md
-"""
-
-import importlib.metadata
-
-# Fetches the version of the package as defined in pyproject.toml
-__version__ = importlib.metadata.version(__package__)
