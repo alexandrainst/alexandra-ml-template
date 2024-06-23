84fdab0a514aa23bf05666bf355afe15f59a72c3
saattrupdan <saattrupdan@gmail.com>
2024-06-23 15:38:27 +0200

    fix: Remove quotes around .env values

--- {{cookiecutter.project_name}}/src/scripts/fix_dot_env_file.py
+++ {{cookiecutter.project_name}}/src/scripts/fix_dot_env_file.py
@@ -100,7 +100,7 @@ def fix_dot_env_file(non_interactive: bool, include_openai: bool) -> None:
             if value == "" and not non_interactive:
                 value = input(desired_env_vars[env_var])
 
-            f.write(f'{env_var}="{value}"\n')
+            f.write(f"{env_var}={value}\n")
 
     # Remove the name and email file
     name_and_email_path.unlink()
