032b6ab68838bf544e49451666d0a6aa72267978
saattrupdan <saattrupdan@gmail.com>
2026-04-14 16:39:59 +0200

    chore: Remove test_module, move dummy test to tests/__init__.py

--- {{cookiecutter.project_name}}/tests/__init__.py
+++ {{cookiecutter.project_name}}/tests/__init__.py
@@ -1,1 +1,5 @@
 """Test suite for the {{ cookiecutter.project_name }} package."""
+
+def test_dummy() -> None:
+    """Dummy test function, to ensure that pytest works."""
+    assert True
--- {{cookiecutter.project_name}}/tests/test_module.py
+++
@@ -1,6 +0,0 @@
-"""Tests for the `module` module."""
-
-
-def test_dummy() -> None:
-    """Dummy test function."""
-    assert True
