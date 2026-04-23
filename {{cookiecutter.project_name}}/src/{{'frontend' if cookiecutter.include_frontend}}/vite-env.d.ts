4b5211f6a52d0cbae88d89071be0eb1a3e344845
saattrupdan <saattrupdan@gmail.com>
2026-04-23 14:09:00 +0200

    fix: Add shim declaration for vue files

--- {{cookiecutter.project_name}}/src/{{'frontend' if cookiecutter.include_frontend}}/vite-env.d.ts
+++ {{cookiecutter.project_name}}/src/{{'frontend' if cookiecutter.include_frontend}}/vite-env.d.ts
@@ -1,5 +1,11 @@
 /// <reference types="vite/client" />
 
+declare module "*.vue" {
+  import type { DefineComponent } from "vue";
+  const component: DefineComponent<object, object, unknown>;
+  export default component;
+}
+
 declare module "*.css" {
   const content: Record<string, string>;
   export default content;
